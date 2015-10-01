0330fa5bda62caca0d7140900206f8c4e9dc54da
Joshua Davison <icanhasflameburger@gmail.com>
2015-10-01 16:09:23 -0700

    Testing IDEA's pushing mechanics.

--- src/main/java/com/example/examplemod/ExampleMod.java
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -10,11 +10,11 @@ public class ExampleMod
 {
     public static final String MODID = "examplemod";
     public static final String VERSION = "1.0";
-    
+
     @EventHandler
     public void init(FMLInitializationEvent event)
     {
-		// some example code
-        System.out.println("DIRT BLOCK UNLOCALIZED NAME >> "+Blocks.dirt.getUnlocalizedName());
+        // some example code
+        System.out.println("DIRT BLOCK >> "+Blocks.dirt.getUnlocalizedName());
     }
 }
