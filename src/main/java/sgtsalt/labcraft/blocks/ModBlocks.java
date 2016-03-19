ec265554afadbd52fb741a6e87784e7c41c511ee
Wes Cook <im@wescook.ca>
2016-03-19 10:47:25 -0700

    Cleaning up how we instantiate our items/blocks.

--- src/main/java/sgtsalt/labcraft/blocks/ModBlocks.java
+++ src/main/java/sgtsalt/labcraft/blocks/ModBlocks.java
@@ -6,15 +6,15 @@ import net.minecraftforge.fml.common.registry.GameRegistry;
 public final class ModBlocks
 {
 	// Create block instance, can be referred to later (for instance, by renderer)
-	public static Block labMachine;
-	public static Block reagentBottle;
-	public static Block labGlass;
+	public static Block labMachine = new LabMachine("labMachine");
+	public static Block reagentBottle = new ReagentBottle("reagentBottle");
+	public static Block labGlass = new LabGlass("labGlass");
 
 	// Register blocks with Forge
 	public static void createBlocks()
 	{
-		GameRegistry.registerBlock(labMachine = new LabMachine("labMachine"), "lab_machine");
-		GameRegistry.registerBlock(reagentBottle = new ReagentBottle("reagentBottle"), "reagent_bottle");
-		GameRegistry.registerBlock(labGlass = new LabGlass("labGlass"), "lab_glass");
+		GameRegistry.registerBlock(labMachine, "lab_machine");
+		GameRegistry.registerBlock(reagentBottle, "reagent_bottle");
+		GameRegistry.registerBlock(labGlass, "lab_glass");
 	}
 }
--- src/main/java/sgtsalt/labcraft/items/ModItems.java
+++ src/main/java/sgtsalt/labcraft/items/ModItems.java
@@ -6,15 +6,15 @@ import net.minecraftforge.fml.common.registry.GameRegistry;
 public final class ModItems
 {
 	// Create item instance, can be referred to later (for instance, by renderer)
-	public static Item labTool;
-	public static Item vial;
-	public static Item vialSlime;
+	public static Item labTool = new LabTool("labTool");
+	public static Item vial = new Vial("vial");
+	public static Item vialSlime = new VialSlime("vialSlime").setAlwaysEdible();
 
 	// Register items with Forge
 	public static void createItems()
 	{
-		GameRegistry.registerItem(labTool = new LabTool("labTool"), "lab_tool");
-		GameRegistry.registerItem(vial = new Vial("vial"), "vial");
-		GameRegistry.registerItem(vialSlime = new VialSlime("vialSlime").setAlwaysEdible(), "vial_slime");
+		GameRegistry.registerItem(labTool, "lab_tool");
+		GameRegistry.registerItem(vial, "vial");
+		GameRegistry.registerItem(vialSlime, "vial_slime");
 	}
 }
